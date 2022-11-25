4dd23f5a5524ee48b676c6c2131fc46c87e233d8
Denis <klevtsov.ds@mail.ru>
2022-11-25 12:29:49 +0500

    Сделал первый комит

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = new string[3] {"Hello", "2", "world"};
+string[] newarray = new string[array.Length];
+void NewArray(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(array, newarray);
+PrintArray(newarray);
